a39bf93ee66598bb2886c4412ada3e766f36a480
gybcb <gybcb@hotmail.com>
2025-09-21 12:55:51 +0800

    Add Apple Bonjour package to custom packages

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -48,4 +48,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netspeedtest luci-i18n-netspeedtest-z
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier"
 
 #WireGuard
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES wireguard-tools kmod-wireguard luci-proto-wireguard
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES wireguard-tools kmod-wireguard luci-proto-wireguard"
+
+#Apple Bonjour
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES umdns"
